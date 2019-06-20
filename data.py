f97b0ee084055b39c06c680feea00f02ec139a6a
Brett Wilson <drytoastman@gmail.com>
2019-06-19 22:56:25 -0700

    divide up python, start fed report

---
+++ data.py
@@ -0,0 +1,148 @@
+
+import bisect
+import dateutil.parser
+import decimal
+import json
+import os
+import types
+
+from flask import current_app, g
+from google.oauth2 import service_account
+import googleapiclient.discovery
+
+secretfile  = os.path.join(os.getcwd(), 'creds.json')
+credentials = service_account.Credentials.from_service_account_file(secretfile, scopes=['https://www.googleapis.com/auth/spreadsheets.readonly'])
+
+class Config():
+    def __init__(self, sheet):
+        for r in sheet['values']:
+            name = r[0].replace(' ', '_').lower()
+            if name in ('children', 'nannies'):
+                val = list(map(str.strip, r[1].split(',')))
+            elif name.startswith('employer'):
+                val = r[1]
+            else:
+                val = decimal.Decimal(r[1])
+            setattr(self, name, val)
+
+
+class PayPeriod():
+    def __init__(self, header, data):
+        self.data = dict()
+        for name, val in zip(header, data):
+            self.data[name] = val
+
+    def startDate(self):         return dateutil.parser.parse(self.data['Start'])
+    def endDate(self):           return dateutil.parser.parse(self.data['End'])
+    def payDate(self):           return dateutil.parser.parse(self.data['PayDate'])
+    def rates(self, name):       return list(map(decimal.Decimal, map(str.strip, self.data['{} Rates'.format(name)].split(','))))
+    def withholding(self, name): return list(map(int, map(str.strip, self.data['{} Withholding'.format(name)].split(','))))
+    def __repr__(self):          return str(self.__dict__)
+
+    @classmethod
+    def parseSheet(cls, sheet):
+        return sorted([ cls(sheet['values'][0], r) for r in sheet['values'][1:] ], key=lambda x: x.endDate())
+
+
+class TaxTables():
+    def __init__(self, single, married):
+        self.single  = self.parseSheet(single)
+        self.married = self.parseSheet(married)
+
+    def parseSheet(self, sheet):
+        amounts = []
+        allowances = []
+        for row in sheet['values'][1:]:
+            amounts.append(decimal.Decimal(row[0]))
+            allowances.append(list(map(decimal.Decimal, row[1:])))
+
+        return (amounts, allowances)
+
+    def getTax(self, married, allow, extra, gross):
+        if married:
+            amounts = self.married[0]
+            allowances = self.married[1]
+        else:
+            amounts = self.single[0]
+            allowances = self.single[1]
+
+        return allowances[bisect.bisect_left(amounts, gross)-1][allow] + extra
+
+
+class Hours():
+    def __init__(self, header, row):
+        self.data = dict()
+        for name, val in zip(header, row):
+            if name == 'Day':
+                pass
+            elif name == 'Date':
+                self.date = dateutil.parser.parse(val)
+            else:
+                self.data[name] = val and decimal.Decimal(val) or decimal.Decimal(0)
+
+    def date(self): return self.date
+    def hours(self, name): return self.data.get(name, decimal.Decimal())
+    def __repr__(self): return str(self.__dict__)
+
+    @classmethod
+    def parseSheet(cls, sheet):
+        return [ cls(sheet['values'][0], r) for r in sheet['values'][1:] ]
+
+
+class Reimbursement():
+    def __init__(self, header, row):
+        for name, val in zip(header, row):
+            if name == 'Date': self.date = dateutil.parser.parse(val)
+            elif name == 'Amount': self.amount = decimal.Decimal(val)
+            elif name == 'Notes': self.notes = val
+    def __repr__(self): return str(self.__dict__)
+
+    @classmethod
+    def parseSheet(cls, sheet):
+        return [ cls(sheet['values'][0], r) for r in sheet['values'][1:] ]
+
+
+def get_api():
+    if not hasattr(g, 'api'):
+        service = googleapiclient.discovery.build('sheets', 'v4', credentials=credentials)
+        g.api   = service.spreadsheets()
+    return g.api
+
+
+def get_config_data():
+    if current_app.config['ENV'] == 'development' and os.path.isfile('config.json'):
+        with open('config.json', 'r') as fp:
+            config = json.load(fp)
+    else:
+        config = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['Config', 'PayPeriods']).execute()
+        if current_app.config['ENV'] == 'development':
+            with open('config.json', 'w') as fp:
+                json.dump(config, fp)
+    return Config(config['valueRanges'][0]), PayPeriod.parseSheet(config['valueRanges'][1])
+
+
+def get_tax_data():
+    if current_app.config['ENV'] == 'development' and os.path.isfile('tax.json'):
+        with open('tax.json', 'r') as fp:
+            tax = json.load(fp)
+    else:
+        tax = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['Single Bracket', 'Married Bracket']).execute()
+        if current_app.config['ENV'] == 'development':
+            with open('tax.json', 'w') as fp:
+                json.dump(tax, fp)
+
+    return TaxTables(*tax['valueRanges'])
+
+
+def get_nanny_data(name):
+    if current_app.config['ENV'] == 'development' and os.path.isfile('{}.json'.format(name)):
+        with open('{}.json'.format(name), 'r') as fp:
+            data = json.load(fp)
+    else:
+        data = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['{} Hours'.format(name), '{} Reimbursements'.format(name)]).execute()
+        if current_app.config['ENV'] == 'development':
+            with open('{}.json'.format(name), 'w') as fp:
+                json.dump(data, fp)
+
+    return types.SimpleNamespace(hours = Hours.parseSheet(data['valueRanges'][0]), reimbursements = Reimbursement.parseSheet(data['valueRanges'][1]))
+
--- main.py
+++ main.py
@@ -1,315 +1,68 @@
 #!/usr/bin/env python3
 
-import bisect
-import collections
-import decimal
-from functools import partial
-import json
-import logging
-import os
-import types
-
+from collections import defaultdict
 import dateutil.parser
-from flask import current_app, Flask, g, redirect, render_template, request, session, url_for
-import googleapiclient.discovery
-from google.oauth2 import service_account
+import decimal
+import os
 
+from flask import Flask, render_template
 
-secretfile  = os.path.join(os.getcwd(), 'creds.json')
-credentials = service_account.Credentials.from_service_account_file(secretfile, scopes=['https://www.googleapis.com/auth/spreadsheets.readonly'])
+from data import *
+from calc import *
+
 app = Flask("nanny-reports")
-
-class Config():
-    def __init__(self, sheet):
-        for r in sheet['values']:
-            name = r[0].replace(' ', '_').lower()
-            if name in ('children', 'nannies'):
-                val = list(map(str.strip, r[1].split(',')))
-            elif name.startswith('employer'):
-                val = r[1]
-            else:
-                val = decimal.Decimal(r[1])
-            setattr(self, name, val)
-
-
-class PayPeriod():
-    def __init__(self, header, data):
-        self.data = dict()
-        for name, val in zip(header, data):
-            self.data[name] = val
-
-    def startDate(self):         return dateutil.parser.parse(self.data['Start'])
-    def endDate(self):           return dateutil.parser.parse(self.data['End'])
-    def payDate(self):           return dateutil.parser.parse(self.data['PayDate'])
-    def rates(self, name):       return list(map(decimal.Decimal, map(str.strip, self.data['{} Rates'.format(name)].split(','))))
-    def withholding(self, name): return list(map(int, map(str.strip, self.data['{} Withholding'.format(name)].split(','))))
-    def __repr__(self):          return str(self.__dict__)
-
-    @classmethod
-    def parseSheet(cls, sheet):
-        return [ cls(sheet['values'][0], r) for r in sheet['values'][1:] ]
-
-
-class TaxTables():
-    def __init__(self, single, married):
-        self.single  = self.parseSheet(single)
-        self.married = self.parseSheet(married)
-
-    def parseSheet(self, sheet):
-        amounts = []
-        allowances = []
-        for row in sheet['values'][1:]:
-            amounts.append(decimal.Decimal(row[0]))
-            allowances.append(list(map(decimal.Decimal, row[1:])))
-
-        return (amounts, allowances)
-
-    def getTax(self, married, allow, extra, gross):
-        if married:
-            amounts = self.married[0]
-            allowances = self.married[1]
-        else:
-            amounts = self.single[0]
-            allowances = self.single[1]
-
-        return allowances[bisect.bisect_left(amounts, gross)-1][allow] + extra
-
-
-class Hours():
-    def __init__(self, header, row):
-        self.data = dict()
-        for name, val in zip(header, row):
-            if name == 'Day':
-                pass
-            elif name == 'Date':
-                self.date = dateutil.parser.parse(val)
-            else:
-                self.data[name] = val and decimal.Decimal(val) or decimal.Decimal(0)
-
-    def date(self): return self.date
-    def hours(self, name): return self.data.get(name, decimal.Decimal())
-    def __repr__(self): return str(self.__dict__)
-
-    @classmethod
-    def parseSheet(cls, sheet):
-        return [ cls(sheet['values'][0], r) for r in sheet['values'][1:] ]
-
-
-class Reimbursement():
-    def __init__(self, header, row):
-        for name, val in zip(header, row):
-            if name == 'Date': self.date = dateutil.parser.parse(val)
-            elif name == 'Amount': self.amount = decimal.Decimal(val)
-            elif name == 'Notes': self.notes = val
-    def __repr__(self): return str(self.__dict__)
-
-    @classmethod
-    def parseSheet(cls, sheet):
-        return [ cls(sheet['values'][0], r) for r in sheet['values'][1:] ]
-
-
-def get_api():
-    if not hasattr(g, 'api'):
-        service = googleapiclient.discovery.build('sheets', 'v4', credentials=credentials)
-        g.api   = service.spreadsheets()
-    return g.api
-
-
-def get_config_data():
-    if current_app.config['ENV'] == 'development' and os.path.isfile('config.json'):
-        with open('config.json', 'r') as fp:
-            config = json.load(fp)
-    else:
-        config = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['Config', 'PayPeriods']).execute()
-        if current_app.config['ENV'] == 'development':
-            with open('config.json', 'w') as fp:
-                json.dump(config, fp)
-    return Config(config['valueRanges'][0]), PayPeriod.parseSheet(config['valueRanges'][1])
-
-
-def get_tax_data():
-    if current_app.config['ENV'] == 'development' and os.path.isfile('tax.json'):
-        with open('tax.json', 'r') as fp:
-            tax = json.load(fp)
-    else:
-        tax = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['Single Bracket', 'Married Bracket']).execute()
-        if current_app.config['ENV'] == 'development':
-            with open('tax.json', 'w') as fp:
-                json.dump(tax, fp)
-
-    return TaxTables(*tax['valueRanges'])
-
-
-def get_nanny_data(name):
-    if current_app.config['ENV'] == 'development' and os.path.isfile('{}.json'.format(name)):
-        with open('{}.json'.format(name), 'r') as fp:
-            data = json.load(fp)
-    else:
-        data = get_api().values().batchGet(spreadsheetId=current_app.config['SPREADSHEET_ID'], ranges=['{} Hours'.format(name), '{} Reimbursements'.format(name)]).execute()
-        if current_app.config['ENV'] == 'development':
-            with open('{}.json'.format(name), 'w') as fp:
-                json.dump(data, fp)
-
-    return types.SimpleNamespace(hours = Hours.parseSheet(data['valueRanges'][0]), reimbursements = Reimbursement.parseSheet(data['valueRanges'][1]))
-
-
-SING   = 0
-BOTH   = 1
-SINGOT = 2
-BOTHOT = 3
-
-def sr(index, hours, rates):
-    return hours * rates[index]
-
-def srh(index, hours, rates):
-    return sr(index, hours, rates)/decimal.Decimal(2)
-
-def hr(hours, rates):
-    return (min(hours,8) * rates[BOTH]) + (max(hours-8,0) * rates[SING])
-
-def hrh(hours, rates):
-    return hr(hours, rates)/decimal.Decimal(2)
-
-
-def dpercent(val):
-    if val is None: return ""
-    if type(val) is str: return val
-    return "{:.2f}%".format(val*100)
-
-def dollar(val, twoplaces=False):
-    if not val: return ""
-    if type(val) is str: return val
-    if isinstance(val, collections.Iterator): return ','.join(map(str,val))
-    if twoplaces:
-        return "${:,.2f}".format(val)
-    return "${:,}".format(val)
-
-def nozero(val):
-    if not val: return ""
-    return val
-
-CENTS = decimal.Decimal('0.01')
-
-def calculate(sconfig, periods, taxtables, name, ndata):
-
-    ret = dict()
-    calcs = list()
-
-    for child in sconfig.children:
-        #             Input Hours Key,  Rate Function,        Destination Keys
-        calcs.append(   ('Both',        partial(srh, BOTH),   [child + ' Gross', child + ' Both']))
-        calcs.append(   ('Both OT',     partial(srh, BOTHOT), [child + ' Gross', child + ' Both OT']))
-        calcs.append(   ('Sick',        hrh,                  [child + ' Gross', child + ' Sick']))
-        calcs.append(   ('Holiday',     hrh,                  [child + ' Gross', child + ' Holiday']))
-        calcs.append(   (child,         partial(sr, SING),    [child + ' Gross', child]))
-        calcs.append(   (child + ' OT', partial(sr, SINGOT),  [child + ' Gross', child + ' OT']))
-
-
-    for period in periods:
-        start = period.startDate()
-        end   = period.endDate()
-        rates = period.rates(name)
-
-        ret[end] = dict()
-        p = ret[end]['hours'] = collections.defaultdict(decimal.Decimal)
-        s = ret[end]['sums'] = collections.defaultdict(decimal.Decimal)
-
-        for h in ndata.hours:
-            if h.date > end: break
-
-            # Full YTD calculations
-            for hrkey, ratefunc, dkeys in calcs:
-                for dkey in dkeys:
-                    p[dkey + ' YTD'] += h.hours(hrkey)
-                    s[dkey + ' YTD'] += ratefunc(h.hours(hrkey), rates)
-
-            if h.date < start: continue
-
-            # Just current period calculations
-            for hrkey, ratefunc, dkeys in calcs:
-                for dkey in dkeys:
-                    p[dkey] += h.hours(hrkey)
-                    s[dkey] += ratefunc(h.hours(hrkey), rates)
-
-        for r in ndata.reimbursements:
-            if r.date > end: break
-            # Full YTD calculations
-            for child in sconfig.children:
-                s[child+' Reimbursements YTD'] += r.amount/2
-
-            if r.date < start: continue
-            for child in sconfig.children:
-                s[child+' Reimbursements'] += r.amount/2
-
-
-    ytd = collections.defaultdict(decimal.Decimal)
-    for period in periods:
-        start = period.startDate()
-        end   = period.endDate()
-        rates = period.rates(name)
-
-        sums = ret[end]
-        w4   = period.withholding(name)
-        s    = sums['sums']
-        t    = sums['tax'] = collections.defaultdict(decimal.Decimal)
-
-        totalgross = decimal.Decimal(0)
-        for child in sconfig.children:
-            totalgross += s[child+' Gross']
-        fed = taxtables.getTax(w4[0], w4[1], w4[2], totalgross)  # fed is calculated as 1 and then divided between employers
-
-        for child in sconfig.children:
-            childgross = s[child+' Gross']
-            fedtax = 0
-            if totalgross: fedtax = ((childgross / totalgross) * fed).quantize(CENTS)
-            medicare = ((childgross * sconfig.medicare)/2).quantize(CENTS)
-            ss       = ((childgross * sconfig.social_security)/2).quantize(CENTS)
-            waleave  =  (childgross * sconfig.family_leave).quantize(CENTS)
-            waunemp  =  (childgross * sconfig.wa_unemployment).quantize(CENTS)
-            fedunemp =  (childgross * sconfig.fed_unemployment).quantize(CENTS)
-
-            employee = fedtax   + ss + medicare + waleave 
-            employer = fedunemp + ss + medicare + waunemp
-
-            # rolling count
-            ytd[child+' Fed']       += fedtax
-            ytd[child+' SS1']       += ss
-            ytd[child+' SS2']       += ss
-            ytd[child+' Medicare1'] += medicare
-            ytd[child+' Medicare2'] += medicare
-            ytd[child+' WALeave']  += waleave
-            ytd[child+' WAUnemp']  += waunemp
-            ytd[child+' FedUnemp'] += fedunemp
-            ytd[child+' EmployeeTax'] += employee
-            ytd[child+' EmployerTax'] += employer
-
-            # child
-            t[child+' Fed']       = fedtax
-            t[child+' SS1']       = ss
-            t[child+' SS2']       = ss
-            t[child+' Medicare1'] = medicare
-            t[child+' Medicare2'] = medicare
-            t[child+' WALeave']   = waleave
-            t[child+' WAUnemp']   = waunemp
-            t[child+' FedUnemp']  = fedunemp
-            t[child+' EmployeeTax'] = employee
-            t[child+' EmployerTax'] = employer
-            for copy in ('Fed', 'SS1', 'SS2', 'Medicare1', 'Medicare2', 'WALeave', 'WAUnemp', 'FedUnemp', 'EmployeeTax', 'EmployerTax'):
-                t['{} {} YTD'.format(child, copy)] = ytd['{} {}'.format(child, copy)]
-
-    return ret
-
 
 @app.route('/')
 def index():
+    sconfig, periods = get_config_data()
+    return render_template("selector.html", sconfig=sconfig, periods=periods)
+
+@app.route('/fed')
+def fed():
+    sconfig, periods = get_config_data()
+    taxtables = get_tax_data()
+    lastp     = periods[-1].endDate()
+    data      = dict()
+
+    ndata = dict()
+    for name in sconfig.nannies:
+        ndata[name] = nanny_calculate(sconfig, periods, taxtables, name, get_nanny_data(name))
+
+
+    for child in sconfig.children:
+        data[child] = dict()
+        data[child]['scheduleH'] = defaultdict(decimal.Decimal)
+        for nanny in sconfig.nannies:
+            data[child][nanny] = dict()
+            data[child][nanny]['gross']    = ndata[nanny][lastp]['sums'][child+' Gross YTD']
+            data[child][nanny]['ss']       = ndata[nanny][lastp]['tax'][child+' SS1 YTD']
+            data[child][nanny]['medicare'] = ndata[nanny][lastp]['tax'][child+' Medicare1 YTD']
+            data[child][nanny]['fed']      = ndata[nanny][lastp]['tax'][child+' Fed YTD']
+
+            data[child]['scheduleH']['gross']     += ndata[nanny][lastp]['sums'][child+' Gross YTD']
+            data[child]['scheduleH']['futagross'] += min(ndata[nanny][lastp]['sums'][child+' Gross YTD'], sconfig.fed_unemployment_base)
+            data[child]['scheduleH']['fed']       += ndata[nanny][lastp]['tax'][child+' Fed YTD']
+            data[child]['scheduleH']['waunemp']   += ndata[nanny][lastp]['tax'][child+' WAUnemp YTD']
+
+        data[child]['scheduleH']['ss']       = (data[child]['scheduleH']['gross']     * sconfig.social_security).quantize(CENTS)
+        data[child]['scheduleH']['medicare'] = (data[child]['scheduleH']['gross']     * sconfig.medicare).quantize(CENTS)
+        data[child]['scheduleH']['futa']     = (data[child]['scheduleH']['futagross'] * sconfig.fed_unemployment).quantize(CENTS)
+
+    return render_template('fed.html', sconfig=sconfig, data=data)
+
+@app.route('/wa')
+def wa():
+    return render_template('wa.html')
+
+@app.route('/paystub/<enddate>/<nannyname>')
+def paystub(enddate, nannyname):
+
+    enddate   = dateutil.parser.parse(enddate.replace('_','/')) 
 
     sconfig, periods = get_config_data()
-    enddate   = dateutil.parser.parse('6/16/19')
-    nannyname = sconfig.nannies[0]
-
     taxtables = get_tax_data()
     ndata     = get_nanny_data(nannyname)
-    results   = calculate(sconfig, periods, taxtables, nannyname, ndata)
+    results   = nanny_calculate(sconfig, periods, taxtables, nannyname, ndata)
 
     period  = next(p for p in periods if p.endDate() == enddate)
     hours   = [h for h in ndata.hours if period.startDate() <= h.date <= period.endDate()]
@@ -317,16 +70,27 @@ def index():
     psums   = results[enddate]
     rates   = period.rates(nannyname)
 
-    nets = dict()
-    for child in sconfig.children:
-        ncalc = [(child, child+' ', ''), (child+' YTD', child+' ', ' YTD')]
-        for dest, prefix, suffix in ncalc:
-            nets[dest] = (psums['sums'][prefix+'Gross'+suffix] - psums['tax'][prefix+'EmployeeTax'+suffix] + psums['sums'][prefix+'Reimbursements'+suffix]).quantize(CENTS, rounding=decimal.ROUND_UP)
-
-    return render_template('paystub.html', sums=psums, nets=nets, hours=hours, reimb=reimb, period=period, rates=rates, sconfig=sconfig, nanny=nannyname, children=sconfig.children)
+    return render_template('paystub.html', sums=psums, hours=hours, reimb=reimb, period=period, rates=rates, sconfig=sconfig, nanny=nannyname, children=sconfig.children)
 
 
 def common_init():
+    def dpercent(val):
+        if val is None: return ""
+        if type(val) is str: return val
+        return "{:.2f}%".format(val*100)
+
+    def dollar(val, twoplaces=False):
+        if not val: return ""
+        if type(val) is str: return val
+        if isinstance(val, collections.Iterator): return ','.join(map(str,val))
+        if twoplaces:
+            return "${:,}".format(val.quantize(CENTS))
+        return "${:,}".format(val)
+
+    def nozero(val):
+        if not val: return ""
+        return val
+
     app.config.from_envvar('SETTINGS_FILE')
     app.jinja_env.filters['dpercent'] = dpercent
     app.jinja_env.filters['dollar']   = dollar
